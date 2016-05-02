3b4f0e027ed6bb20a19c15b13c19c3b51b4a49d4
cecilia.cho <cecilia.cho@lge.com>
2016-05-02 14:51:48 +0900

    약수의 합 / Large set통과

--- Previous_divisor/src/Divisor.java
+++ Previous_divisor/src/Divisor.java
@@ -19,89 +19,54 @@ public class Divisor {
 		for (int i = 0; i < tc; i++) {
 			int num1 = sc.nextInt();
 			int num2 = sc.nextInt();
+
 			sol.setRange(num1, num2);
-			sol.cal();
+			int count = sol.cal();
+			System.out.println(count);
+			wr.write(count + "\n");
 		}
 
-		// File write
-		wr.write(" ");
-		// }
-
 		sc.close();
+		wr.close();
 	}
 
 	class Solution {
 		int from;
 		int to;
-		int memo[];
 
 		public Solution() {
-			//memo = new int[500000];
 		}
 
 		public void setRange(int from, int to) {
 			this.from = from;
 			this.to = to;
 		}
-		
+
 		// 16 약수 - {1 2 4 8 16} 4까지만 검사하면 / 로 알수있음
 		// 10 약수 - {1 2 5 10} 2까지 검사하면 / 로 알수있음
 		// for loop 를 i*i <= n까지 돌림
 		// 루트16 = 4 까지 루프를 돌려서 n/i + i
 
-		public void cal() {
+		public int cal() {
 			int count = 0;
 			for (int n = from; n <= to; n++) {
-				
+
 				int sum = 1;
-				for(int i=2; i*i<=n ; i++) { 
-					if (n%i == 0) {
-						sum += n/i;
-						if (i*i!=n)
-							sum+=i;
+				for (int i = 2; i * i <= n; i++) {
+					if (n % i == 0) {
+						sum += n / i;
+						if (i * i != n)
+							sum += i;
 					}
 				}
-				
-				//System.out.println(n + " " + sum);
+
+				// System.out.println(n + " " + sum);
 
 				if (sum >= n)
 					count++;
 			}
-			
-			System.out.println(count);
+
+			return count;
 		}
-		
-//		public void cal() {
-//			Arrays.fill(memo, 0);
-//			int count = 0;
-//			for (int n = from; n <= to; n++) {
-//				memo[0] = memo[1] = 0;
-//
-//				if (n > 1) {
-//					int sum = 1;
-//
-//					if (memo[n / 2] == 0) {
-//						for (int i = 2; i <= n / 2; i++) {
-//							if (n % i == 0) {
-//								memo[n/2] = sum = sum + n / i;
-//							}
-//						}
-//					} else {
-//						if (n > 6 && n % 6 == 0) {// 6 배수라면
-//							sum = memo[n / 2] + n / 2 + n / 3;
-//						} else if (n > 2 && n % 2 == 0) // 2의 배수라면,
-//							sum = memo[n / 2] + n / 2;
-//						else if (n > 3 && n % 3 == 0) // 3의 배수라면,
-//							sum = memo[n / 3] + n / 3;
-//					}
-//
-//					System.out.println(n + " " + sum);
-//
-//					if (sum >= n)
-//						count++;
-//				}
-//			}
-//			// System.out.println(count);
-//		}
 	}
 }
