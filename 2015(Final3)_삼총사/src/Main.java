95431c0f3ec32eb79e440dae8f626b468eb88c41
cecilia.cho <cecilia.cho@lge.com>
2016-05-04 11:52:14 +0900

    2015 f 3

---
+++ 2015(Final3)_삼총사/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(new File("sample.in"));
+		FileWriter wr = new FileWriter(new File("problem.out"));
+		Main m = new Main();
+		
+		int tc = sc.nextInt();
+		while(tc-- > 0) {
+			// input tc #
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			int W = sc.nextInt();
+			
+			Solution sol = m.new Solution();
+			for (int i = 0; i < N; i++) {
+				int p1 = sc.nextInt();
+				int p2 = sc.nextInt();
+			}
+			
+			// File write
+			wr.write(" ");
+		}
+		
+		sc.close();
+		wr.close();
+	}
+	
+	class Solution {
+		public Solution() {
+			
+		}
+	}
+}
